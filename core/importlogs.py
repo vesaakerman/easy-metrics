f962831fe8b2230db3de2593568c0ac755438caa
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2017-03-13 13:37:06 +0000

    Import of logs updated

--- core/importlogs.py
+++ core/importlogs.py
@@ -1,99 +1,35 @@
 #!/usr/bin/python
-# coding: utf-8
+from __future__ import print_function, absolute_import
 
-# In[67]:
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 import re
-from pymongo import MongoClient
 from os import walk
+from pymongo import MongoClient
+from easy.settings import *
+from easy.core.database import *
+from pprint import pprint 
 
-path = "/Users/vyacheslavtykhonov/projects/management"
-#testfilename = "statistics.log.2016-09-26"
+path = "%s/../../tests" % HERE
+logspath = "%s/logs" % path
+print("Importing logs from %s" % logspath)
 
 client = MongoClient()
 management = client.get_database('management')
 col = management.data
 
 f = []
-for (dirpath, dirnames, filenames) in walk("%s/logs" % path):
+for (dirpath, dirnames, filenames) in walk("%s" % logspath):
+    pprint(filenames)
     f.extend(filenames)
 
 for filename in f:
     fullpath = "%s/logs/%s" % (path, filename)
     outpath = "%s/reports/%s.sum" % (path, filename)
-    print fullpath
-    file2mongo(fullpath)
+    print(fullpath)
+    file2mongo(fullpath, col)
 
-
-# In[68]:
-
-def cleanline(thisline):
-    l = re.compile("\s+\;\s+").split(thisline)
-    maininfo = {}
-    for i in range(0,5):
-#        print l[i]
-        maininfo[str(i)] = l[i]
-    
-    # date, time and action
-    attributes = re.search(r"(\d{4}\-\d+\-\d+)\s+(\S+)\,\d+\s+\-\s+(.+)", maininfo["0"])
-    if attributes:
-        maininfo["date"] = attributes.group(1)
-        maininfo["time"] = attributes.group(2)
-        maininfo["action"] = attributes.group(3)
-        maininfo["user"] = maininfo["1"]
-        
-    # datetime - action; user; roles; groups; ip
-    l = l[5:]
-    
-    # dataset description
-    for item in l:
-        try:
-            info = re.compile("\:\s+").split(item)
-            info[1] = info[1].replace('"', '')
-            info[1] = re.sub(r'\)$', '', info[1])
-            maininfo[info[0]] = info[1]
-            #print "%s = %s" % (info[0], info[1])
-        except:
-            skip = item
-    
-    return (maininfo, l)
-
-
-
-# In[69]:
-
-def file2mongo(path):
-    file = open(fullpath, 'r')
-    outfile = open(outpath,'w')
-    lastline = file.readline()
-
-
-    intstats = {}
-    for lastline in file:
-        lastline = lastline[:-2]
-        l = re.compile("\s+\;\s+").split(lastline)
-        try:
-            intstats[str(len(l))] = intstats[str(len(l))] + 1
-        except:
-            intstats[str(len(l))] = 1
-    
-        # Try to recognize datasets
-        finddataset = re.search(r"(easy\-dataset\:(\d+))", lastline) 
-        dataset = '0'
-        if finddataset:
-            dataset = finddataset.group(0)
-        report = "%s %s %s\n" % (str(l[4]), str(len(l)), dataset)
-    
-        if (len(l) > 20000):
-            print str(l)
-    
-        (main, newline) = cleanline(lastline)
-        col.insert_one(main)
-        outfile.write(report)
-    outfile.close()
-
-
-# In[70]:
-
-print "Done"
+print("Import is finished.")
 
