516bc8835439d43de51d02b50ad5bf527f66ae2d
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2017-03-13 13:57:29 +0000

    EASY settings adjusted

--- easy/settings/__init__.py
+++ easy/settings/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
 
-from parthenos.settings.defaults import *  # noqa
+from easy.settings.defaults import *  # noqa
 
 try:
-    from parthenos.settings.local import *  # noqa
+    from easy.settings.local import *  # noqa
 except ImportError as error:
     pass
